30e50d2079882b8cb4fd0d20f30790bbf037769a
Alex-Asim <57575430+Alex-Asim@users.noreply.github.com>
2024-01-31 21:02:58 +0200

    Add files via upload

---
+++ student-management-system/student-management-system/src/main/java/net/javaguides/sms/controller/StudentController.java
@@ -0,0 +1,28 @@
+package net.javaguides.sms.controller;
+
+import net.javaguides.sms.dto.StudentDto;
+import net.javaguides.sms.service.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    // handler method to handle list students request
+    @GetMapping("/students")
+    public String listStudents(Model model){
+        List<StudentDto> students = studentService.getAllStudents();
+        model.addAttribute("students", students);
+        return "students";
+    }
+
+}
