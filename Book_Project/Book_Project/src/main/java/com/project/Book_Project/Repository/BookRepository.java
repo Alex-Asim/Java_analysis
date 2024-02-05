85df19ebf2d480c9417c1ed21ff37d431a902e51
Alex-Asim <57575430+Alex-Asim@users.noreply.github.com>
2024-02-06 00:05:07 +0200

    Delete Book_Project/Book_Project directory

--- Book_Project/Book_Project/src/main/java/com/project/Book_Project/Repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.Book_Project.Repository;
-
-import com.project.Book_Project.Entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
