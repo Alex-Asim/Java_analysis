caf7a319e58787bcd84f72bf4e61cda01f4da523
Alex-Asim <57575430+Alex-Asim@users.noreply.github.com>
2024-02-06 00:30:09 +0200

    Update StudentService.java

--- student-management-system/student-management-system/src/main/java/net/javaguides/sms/service/StudentService.java
+++ student-management-system/student-management-system/src/main/java/net/javaguides/sms/service/StudentService.java
@@ -1,10 +1,8 @@
 package net.javaguides.sms.service;
 
 import net.javaguides.sms.dto.StudentDto;
-
 import java.util.List;
 
 public interface StudentService {
     List<StudentDto> getAllStudents();
-
 }
